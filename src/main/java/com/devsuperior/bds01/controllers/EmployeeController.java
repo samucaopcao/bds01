4cd5f857f7a0781ec39dc502a3305b5ba04a0cbd
Samuel M Santos <samuelmacedo2011@gmail.com>
2022-06-06 17:16:22 -0300

    Termino de implementação de desafio resolvido

--- src/main/java/com/devsuperior/bds01/controllers/EmployeeController.java
+++ src/main/java/com/devsuperior/bds01/controllers/EmployeeController.java
@@ -1,5 +1,7 @@
 package com.devsuperior.bds01.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -7,8 +9,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.bds01.dto.EmployeeDTO;
 import com.devsuperior.bds01.services.EmployeeService;
@@ -26,6 +31,18 @@ import com.devsuperior.bds01.services.EmployeeService;
 			PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("name"));
 			Page<EmployeeDTO> list = service.findAll(pageRequest);
 			return ResponseEntity.ok().body(list);
+		}
+		
+		@PostMapping
+		public ResponseEntity<EmployeeDTO> insert(@RequestBody EmployeeDTO dto) {
+			dto = service.insert(dto);
+
+			// Restornará o codigo 201 e também um parametro adicional no cabeçalho da
+			// resposta
+			// que será o endereço do novo recurso criado
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/categories/{id}").buildAndExpand(dto.getId())
+					.toUri();
+			return ResponseEntity.created(uri).body(dto);
 		}
 	
 	
